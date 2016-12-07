a231c98b00d3427e112cc8cc944fc40c559157e1
Fredrik Forsmo <frozzare@users.noreply.github.com>
2016-12-07 13:14:36 +0100

    Fix cli script

--- src/cli.php
+++ src/cli.php
@@ -16,17 +16,12 @@ WP_CLI::add_hook( 'after_wp_load', function () {
 	}
 
 	// Subdomains are required.
-	if ( ! is_subdomain_install() ) {
+	if ( ! function_exists( 'is_subdomain_install' ) || ! is_subdomain_install() ) {
 		WP_CLI::error( 'Hercules requires subdomains mode' );
 	}
 
-	// Support `domain` arg even if `wp site create` don't.
-	if ( isset( WP_CLI::get_runner()->assoc_args['domain'] ) ) {
-		$domain = WP_CLI::get_runner()->assoc_args['domain'];
-		unset( WP_CLI::get_runner()->assoc_args['domain'] );
-	} else {
-		$domain = WP_CLI::get_runner()->assoc_args['slug'];
-	}
+	// Get domain.
+	$domain = isset( WP_CLI::get_runner()->assoc_args['slug'] ) ? WP_CLI::get_runner()->assoc_args['slug'] : '';
 
 	// Not a valid host.
 	if ( parse_url( 'http://' . $domain, PHP_URL_HOST ) !== $domain ) {
@@ -39,14 +34,11 @@ WP_CLI::add_hook( 'after_wp_load', function () {
 	// Split the domain.
 	$domain = explode( '.', $domain );
 
-	// Can only work with two parts.
-	if ( count( $domain ) !== 2 ) {
-		WP_CLI::error( 'Hercules requires a valid top domain, e.g example.com' );
-	}
-
-	// Set new slug in WP CLI.
-	WP_CLI::get_runner()->assoc_args['slug'] = $domain[0];
-
 	// Set current site domain.
-	$current_site->domain = $domain[1];
+	$current_site->domain = 'dev';
+
+	// Set new slug.
+	WP_CLI::get_runner()->run_command( ['site', 'create'], ['slug' => $domain[0]] );
+
+	exit;
 } );
